fb2d2298cf8e22e2115bf628021ba68752929b47
ignat99 <ignat99@spain.holding.bz>
2018-12-01 13:47:57 +0200

    涼宮ハルヒ

--- heap_sutras_real_2.c
+++ heap_sutras_real_2.c
@@ -21,21 +21,21 @@ int FixTree(int iFrom,int iTo)
 	TElement  elTop,elLeft,elRight;
 	int  iFromTwice;
 
- 	elTop=elA[iFrom];
+	elTop=elA[iFrom];
 
 	while(1) {
- 		iFromTwice=iFrom*2;
- 		if (iFromTwice > iTo) break; 
- 		else {
+		iFromTwice=iFrom*2;
+		if (iFromTwice > iTo) break; 
+		else {
 			elLeft=elA[iFromTwice];
 			if (iFromTwice==iTo) { elRight=elLeft; } else  { elRight=elA[iFromTwice+1]; }
 
 			if ((elRight>elTop) && (elRight>elLeft)) { elA[iFrom]=elRight;  iFrom=iFromTwice+1;  } 
 			else if (elLeft > elTop ) { elA[iFrom]=elLeft;   iFrom=iFromTwice; } 
 			else break; 
- 		}
+		}
 	}
- 	elA[iFrom]=elTop;
+	elA[iFrom]=elTop;
 }
 
 void main (int argc, char *argv []) 
@@ -48,9 +48,9 @@ void main (int argc, char *argv [])
   while (iNum < N_MAX-1) {
     if(scanf("%s", str)<=0)break;
     elA[++iNum] = atoi(str);
-  } 
-  
-  
+  }
+
+
 // { Build a sorting tree  }
   for ( i= iNum >> 1;i>=1;i--) {  FixTree(i,iNum);
 
@@ -70,8 +70,8 @@ void main (int argc, char *argv [])
 
 
 // { Convert tree to array }
-  for (i=iNum; i>=2; i-- ) 
-  { 
+  for (i=iNum; i>=2; i-- )
+  {
 	elTemp=elA[1];
 	elA[1]=elA[i];
 	elA[i]=elTemp;
